48b9a107aead58c5a3f9e2d22d892cc9d7b9c769
sxl <364864264@qq.com>
2021-01-28 14:27:40 +0800

    commit

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    404页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error404'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex"
+      mode="horizontal"
+      :router="true"
+      @select="handleSelect"
+    >
+      <el-menu-item index="/">
+        首页
+      </el-menu-item>
+    </el-menu>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      activeIndex: '0'
+    }
+  },
+  methods: {
+    handleSelect () {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

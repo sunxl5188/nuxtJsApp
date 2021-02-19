11284679eadc485006965a7451051eed317906da
sxl <364864264@qq.com>
2021-02-19 11:36:08 +0800

    commit-default

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,12 +1,6 @@
 <template>
   <b-container class="bg-light p-5">
-    <div class="text-center my-3">
-      <b-table striped hover bordered :fields="fields" :items="items">
-        <template #cell(index)="data">
-          {{ data.index + 1 }}
-        </template>
-      </b-table>
-    </div>
+    <div class="text-center my-3" />
   </b-container>
 </template>
 
@@ -14,31 +8,16 @@
 export default {
   name: 'Index',
   data () {
-    return {
-      value: 33.333333333,
-      max: 100,
-      fields: ['first_name', 'last_name', 'age'],
-      items: [
-        { id: 1, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { id: 2, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { id: 3, age: 89, first_name: 'Geneva', last_name: 'Wilson', _rowVariant: 'danger' },
-        { id: 4, age: 38, first_name: 'Jami', last_name: 'Carney', _rowVariant: 'success' }
-      ]
-    }
+    return { }
   },
-  computed: {},
+  computed: {
+  },
   beforeCreate () {
 
   },
   mounted () {
   },
   methods: {
-    showModal () {
-      this.$refs.my.show()
-    },
-    linkGen (pageNum) {
-      return pageNum === 1 ? '?' : `?page=${pageNum}`
-    }
   }
 }
 </script>

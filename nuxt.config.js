30e2fc0f74d5cd4ed5aea36495e454ada294d04f
364864264 <364864264@qq.com>
2022-04-01 14:39:06 +0800

    配置框架

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,16 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxtjs-app',
+    title: '网站标题',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no' },
+      { hid: 'description', name: 'description', content: '网站描述' },
+      { hid: 'keywords', name: 'keywords', content: '网站关键字' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -20,10 +21,10 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/antd-ui'],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // 自动导入组件: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  // 开发和构建模块 (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
@@ -38,9 +39,38 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    proxy: true,
+    prefix: '/api',
+    credentials: true
+  },
+
+  proxy: {
+    '/api': {
+      target: process.env['NUXT_ENV_'+process.env.NODE_ENV.toUpperCase()+'_PROXY'],
+      pathRewrite:
+        {
+          '^/api': '',
+          changeOrigin: true,
+        }
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    analyze: true, // 分析并可视化构建后的打包文件
+    extractCSS: true, // 使用 Vue 服务器端渲染指南启用常见 CSS 提取
+    transpile: [/^element-ui/],
+    vendor: ['axios'] // 为防止重复打包
+  },
+  buildDir: 'nuxt-dist',
+  server: {
+    host: '0.0.0.0', // 默认: localhost
+    port: '3000' // 默认端口号
+  },
+  cli: {
+    bannerColor: 'yellow'
+  },
+  env: {
+    baseUrl: process.env['NUXT_ENV_'+process.env.NODE_ENV.toUpperCase()+'_BASE_URL']
+  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <Tutorial />
+  <div></div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  mounted () {
+    this.$axios.$get('admin/mirror/bannerList')
+  }
 }
 </script>

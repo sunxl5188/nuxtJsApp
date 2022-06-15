d064524c78bf8595319852754617c5e4032a15d8
364864264 <364864264@qq.com>
2022-06-15 18:18:57 +0800

    Signed-off-by: 364864264 <364864264@qq.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="">
-    <input type="file" name="ip" id="ip" @change="onChang">
+    <input type="file" name="fileInput" @change="onChang">
   </div>
 </template>
 
 <script>
   // getDuration
-  import { getVideoDuration, getDuration } from '~/static/js/utils'
+  import { getDuration } from '~/static/js/utils'
 
   export default {
     name: 'IndexPage',
@@ -17,17 +17,23 @@
       }
     },
     computed: {},
-    async mounted () {
+    mounted () {
+      const audio = new Audio('https://oss.szylcn.com/dev/video/20220527/F3C039AD17A8C5EC1653615502853.mp4')
+      audio.muted = false
+      audio.play().then(res => {
+        console.log(res)
+      })
       this.$nextTick(() => {
         // console.log(this.$cookies)
-
+        /* _.delay(async () => {
+          const duration = await getVideoDuration('https://oss.szylcn.com/dev/video/20220527/F3C039AD17A8C5EC1653615502853.mp4')
+          console.log(duration)
+        }, 3000) */
       })
-      const duration = await getVideoDuration('https://oss.szylcn.com/dev/video/20220527/F3C039AD17A8C5EC1653615502853.mp4')
-      console.log(duration)
     },
     methods: {
       async onChang () {
-        const doc = document.getElementById('ip')
+        const doc = document.getElementsByName('fileInput')[0]
         const d = await getDuration(doc.files[0])
         console.log(d)
       }

c78395e395d640e705276e886de774f35d793651
364864264 <364864264@qq.com>
2022-07-04 18:27:30 +0800

    提交更新

--- pages/Admin/Dashboard/WorkPlace.vue
+++ pages/Admin/Dashboard/WorkPlace.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>workplace</div>
+  <div>
+    <a-row>
+      <a-col :xs="24" :sm="24" :md="16" :lg="16" :xl="16" :xxl="16">
+        <MyCard title="进行中的项目">
+          <template #extra>
+            <a-button type="secondary">全部项目</a-button>
+            <a-button type="primary">确定</a-button>
+          </template>
+        </MyCard>
+      </a-col>
+      <a-col :xs="24" :sm="24" :md="8" :lg="8" :xl="8" :xxl="8">2222222</a-col>
+    </a-row>
+  </div>
 </template>
 
 <script>
+  import MyCard from '~/components/MyCard'
+
   export default {
     name: 'WorkPlace',
-    meta: {title: '工作台'}
+    components: { MyCard },
+    meta: { title: '工作台' }
   }
 </script>
 
